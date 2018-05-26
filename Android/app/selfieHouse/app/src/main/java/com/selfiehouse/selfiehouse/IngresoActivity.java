defe5632afccc0b0886870f7aac1f724274214bf
fernandodj <ferjalid@gmail.com>
2018-05-26 16:02:19 -0300

    Add files via upload

---
+++ Android/app/selfieHouse/app/src/main/java/com/selfiehouse/selfiehouse/IngresoActivity.java
@@ -0,0 +1,11 @@
+package com.selfiehouse.project.selfiehouse;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class IngresoActivity {
+
+}
