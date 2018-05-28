b796041ac03a4c7cdec80da3aef420a804852332
Mauro Trotta <mauroatrotta@gmail.com>
2018-05-27 21:08:31 -0300

    Revert "Add files via upload"
    This reverts commit defe5632afccc0b0886870f7aac1f724274214bf.

--- Android/app/selfieHouse/app/src/main/java/com/selfiehouse/selfiehouse/MainActivity.java
+++ Android/app/selfieHouse/app/src/main/java/com/selfiehouse/selfiehouse/MainActivity.java
@@ -1,32 +1,13 @@
-package com.selfiehouse.project.selfiehouse;
+package com.selfiehouse.selfiehouse;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new Handler().post(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
     }
-
 }
