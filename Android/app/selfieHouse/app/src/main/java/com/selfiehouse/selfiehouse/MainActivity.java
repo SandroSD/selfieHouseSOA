738361e4cfd57440ec0118a774c08f102585e026
Mauro Trotta <mauroatrotta@gmail.com>
2018-05-30 23:53:05 -0300

    Botones, Activities y WebView
    Se agregan botones y acciones onClick oara cada boton.
    Se crean Activities para cada boton.
    Se mejora la activity SolicitarAcceso para cargar una pagina dentro de ella misma con WebView.

--- Android/app/selfieHouse/app/src/main/java/com/selfiehouse/selfiehouse/MainActivity.java
+++ Android/app/selfieHouse/app/src/main/java/com/selfiehouse/selfiehouse/MainActivity.java
@@ -1,13 +1,83 @@
 package com.selfiehouse.selfiehouse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button_Cfg,button_Acc, button_SAcc, button_Ctrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button_Acc = (Button)findViewById(R.id.btnAcceder);
+        button_SAcc = (Button)findViewById(R.id.btnSolicitarAcceso);
+        button_Ctrl = (Button)findViewById(R.id.btnControlar);
+        button_Cfg = (Button)findViewById(R.id.btnConfiguracion);
+
+        /* Click listener del boton Acceso */
+
+        button_Acc.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v){
+                // Estoy en MainActivity.this y voy hacia MainActivity.class
+                Intent accesoIntent = new Intent (MainActivity.this, AccesoActivity.class);
+                startActivity(accesoIntent);
+            }
+
+        });
+
+        /* Click listener del boton Solicitar Acceso */
+
+        button_SAcc.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v){
+                // Estoy en MainActivity.this y voy hacia MainActivity.class
+                Intent solicitarAccesoIntent = new Intent (MainActivity.this, SolicitarAccesoActivity.class);
+                startActivity(solicitarAccesoIntent);
+            }
+
+        });
+
+        /* Click listener del boton Controlar */
+
+        button_Ctrl.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v){
+                // Estoy en MainActivity.this y voy hacia MainActivity.class
+                Intent controlIntent = new Intent (MainActivity.this, ControlActivity.class);
+                startActivity(controlIntent);
+            }
+
+        });
+
+
+
+
+        /* Click listener del boton Configuracion */
+
+        button_Cfg.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v){
+                // Estoy en MainActivity.this y voy hacia MainActivity.class
+                Intent configuracionIntent = new Intent (MainActivity.this, ConfiguracionActivity.class);
+                startActivity(configuracionIntent);
+            }
+
+        });
+
+
+    }
+
+    //Metodo que es llamada cuando se cierra la activity
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        //Toast.makeText(getApplicationContext(),"Cerrando Inicio",Toast.LENGTH_LONG).show();
     }
 }
