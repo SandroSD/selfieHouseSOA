37c85d94c01ab0c013c1bc02f8258ae1741dd7c9
Sandro <sdezerio@gmail.com>
2018-06-01 19:35:38 -0300

    cambiamos formato de nombre de fotos

--- Webserver/src/plugins/html5-webcam-save.php
+++ Webserver/src/plugins/html5-webcam-save.php
@@ -4,7 +4,7 @@ $url = $_POST['url'];
 $filteredData = explode(',', $url);
 $unencoded = base64_decode($filteredData[1]);
 
-$datetime = date("Y-m-d-H:i:s", time() ) ; # - 3600*7
+$datetime = date("Y-m-d-H-i-s", time() ) ; # - 3600*7
 
 // name & save the image file 
 $fp = fopen('../images/'.$datetime.'.jpg', 'w');
