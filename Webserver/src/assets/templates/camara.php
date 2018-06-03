6012d1d4da01c12ab4353e1dbb1474c0d224f4cc
Sandro Dezerio <sdezerio@gmail.com>
2018-06-03 15:19:58 -0300

    mensaje de salida al sacar foto, volver a importar BD

--- Webserver/src/assets/templates/camara.php
+++ Webserver/src/assets/templates/camara.php
@@ -1,11 +1,11 @@
 <div class="col-12 col-md-5 col-lg-5 offset-md-3 offset-lg-3">
     <div class="camcontent">
         <video id="video" autoplay></video>
-        <canvas id="canvas" width="640" height="480">
-    </div>
-    <div class="cambuttons">
+        <canvas id="canvas" width="640" height="480">        
+    </div>    
+    <div class="cambuttons">        
         <button class="btn btn-info" id="snap">Capturar Foto</button> 
         <button class="btn btn-warning" id="reset">Reiniciar</button>     
         <button class="btn btn-success" id="upload">Subir</button> 
-    </div> 
+    </div>        
 </div>
