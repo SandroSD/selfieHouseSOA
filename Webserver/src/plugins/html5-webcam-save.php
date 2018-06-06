474fc0ba29f6e1eab8d96b50de9d865352e77ede
Mauro Trotta <mauroatrotta@gmail.com>
2018-06-06 00:08:18 -0300

    Fixes menores de toma de fotos
    Ahora le pega bien a la base de datos y graba.
    El mensaje de confirmacion no lo devuelve bien. Siempre muestra error aunque no lo haya.

--- Webserver/src/plugins/html5-webcam-save.php
+++ Webserver/src/plugins/html5-webcam-save.php
@@ -1,6 +1,6 @@
 <?php
-
-$url = $_POST['url'];
+require_once '../private/Config.php';
+echo $url = $_POST['url'];
 $filteredData = explode(',', $url);
 $unencoded = base64_decode($filteredData[1]);
 
@@ -9,9 +9,9 @@ $datetime = date("Y-m-d-H-i-s", time() ) ; # - 3600*7
 
 $fp = fopen('../images/'.$datetime.'.jpg', 'w');   
 if($fp){
-    $conn = new mysqli("localhost","root","s4ndr0i99i","db_selfiehouse");
+    $conn = new mysqli(SERVER,USER,PASS,DB);
     if($conn){
-        $consulta = "INSERT INTO `db_selfiehouse`.`acceso` (`FECHA`, `USUARIO`, `FOTO`, `ESTADO`) VALUES ('".$datetime."', 'usuario', 'Webserver/src/images/".$datetime.".jpg', 1)";        
+        $consulta = "INSERT INTO `acceso_solicitud` (`FECHA`, `FOTO`, `ESTADO`) VALUES ('".$datetime."',  'Webserver/src/images/".$datetime.".jpg', 1);";        
         if($conn->query($consulta)){
             echo "OK";
             fwrite($fp, $unencoded);
